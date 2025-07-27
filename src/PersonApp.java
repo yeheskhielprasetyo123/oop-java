d29469c432e1bd6313128cfcd8e438585b385f8d
yeheskhielprasetyo <yeheskhielprasetyorakordana123@gmail.com>
2025-07-27 13:26:00 +0700

    class & object

---
+++ src/PersonApp.java
@@ -0,0 +1,15 @@
+public class PersonApp {
+    public static void main(String[] args) {
+
+        // cara membuat object dengan new dan ()
+        var person = new Person();
+        Person person1 = new Person();
+        Person person2;
+        person2 = new Person();
+
+        System.out.println(person);
+        System.out.println(person1);
+        System.out.println(person2);
+
+    }
+}
