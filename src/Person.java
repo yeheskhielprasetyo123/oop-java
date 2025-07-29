baf31b47e945eb011cc27ddca5922f6c0a34a37c
yeheskhielprasetyo <yeheskhielprasetyorakordana123@gmail.com>
2025-07-29 13:40:15 +0700

    class & object

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,24 @@
 class Person {
+    String name;
+    String address;
+    final String country = "Indonesia";
 
+    // constructor method yang pertama kali dipanggil ketika method pertama kali dibuat
+    Person(String paramName, String paramAddress) {
+        name  = paramName;
+        address = paramAddress;
+    }
+
+    Person(String paramName) {
+        name  = paramName;
+    }
+
+    Person() {
+
+    }
+
+    // pake void karna tidak mengembalikan data
+    void sayHello(String paramName) {
+        System.out.println("Hello " + paramName + ", My name is " + name);
+    }
 }
--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,15 +1,22 @@
 public class PersonApp {
     public static void main(String[] args) {
-
         // cara membuat object dengan new dan ()
-        var person = new Person();
-        Person person1 = new Person();
-        Person person2;
-        person2 = new Person();
+        var person = new Person("Eki", "Antang");
+        person.name = "Eki";
+        person.address = "Antang";
 
-        System.out.println(person);
-        System.out.println(person1);
-        System.out.println(person2);
+        System.out.println(person.name);
+        System.out.println(person.address);
+        System.out.println(person.country);
 
+        person.sayHello("Ekoo");
+
+        Person person2 = new Person("Ekii");
+        person2.name = "Ekii";
+        person2.sayHello("Ekaa");
+
+        Person person3 = new Person();
+        person3.sayHello("Ekuuu");
     }
+
 }
